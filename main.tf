2042f2122b1c2deac12c8e8b21da409e56d9aac5
Roger Berlind <roger@hashicorp.com>
2017-11-26 23:41:13 -0500

    using namespace and service account external

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ provider "kubernetes" {
 
 resource "null_resource" "auth_config" {
   provisioner "local-exec" {
-    command = "curl --header \"X-Vault-Token: $VAULT_TOKEN\" --header \"Content-Type: application/json\" --request POST --data '{ \"kubernetes_host\": \"https://${var.k8s_endpoint}:8443\",  \"kubernetes_ca_cert\": \"${chomp(replace(base64decode(var.k8s_master_auth_cluster_ca_certificate), "\n", "\\n"))}\" }' $VAULT_ADDR/v1/auth/${var.vault-k8s-auth-backend}/config"
+    command = "curl --header \"X-Vault-Token: $VAULT_TOKEN\" --header \"Content-Type: application/json\" --request POST --data '{ \"kubernetes_host\": \"${var.k8s_endpoint}\",  \"kubernetes_ca_cert\": \"${chomp(replace(base64decode(var.k8s_master_auth_cluster_ca_certificate), "\n", "\\n"))}\" }' $VAULT_ADDR/v1/auth/${var.vault-k8s-auth-backend}/config"
   }
 }
 
@@ -27,37 +27,37 @@ resource "vault_generic_secret" "role" {
   data_json = <<EOT
   {
     "bound_service_account_names": "cats-and-dogs",
-    "bound_service_account_namespaces": "default",
+    "bound_service_account_namespaces": "cats-and-dogs",
     "policies": "admins",
     "ttl": "2h"
   }
   EOT
 }
 
-resource "kubernetes_namespace" "cats-and-dogs" {
+/*resource "kubernetes_namespace" "cats-and-dogs" {
   metadata {
     name = "cats-and-dogs"
   }
   depends_on = ["vault_generic_secret.role", "null_resource.auth_config" ]
-}
+}*/
 
-resource "kubernetes_service_account" "cats-and-dogs" {
+/*resource "kubernetes_service_account" "cats-and-dogs" {
   metadata {
     name = "cats-and-dogs"
     namespace = "${kubernetes_namespace.cats-and-dogs.metadata.0.name}"
   }
-}
+}*/
 
 resource "kubernetes_pod" "cats-and-dogs-backend" {
   metadata {
     name = "cats-and-dogs-backend"
-    namespace = "${kubernetes_namespace.cats-and-dogs.metadata.0.name}"
+    namespace = "cats-and-dogs"
     labels {
       App = "cats-and-dogs-backend"
     }
   }
   spec {
-    service_account_name = "${kubernetes_service_account.cats-and-dogs.metadata.0.name}"
+    service_account_name = "cats-and-dogs"
     container {
       image = "rberlind/redis-pwd-from-vault:latest"
       name  = "cats-and-dogs-backend"
@@ -70,7 +70,7 @@ resource "kubernetes_pod" "cats-and-dogs-backend" {
         name = "K8S_TOKEN"
         value_from {
           secret_key_ref {
-            name = "${kubernetes_service_account.cats-and-dogs.default_secret_name}"
+            name = "cats-and-dogs-token-mjbl9"
             key = "token"
           }
         }
@@ -85,7 +85,7 @@ resource "kubernetes_pod" "cats-and-dogs-backend" {
 resource "kubernetes_service" "cats-and-dogs-backend" {
   metadata {
     name = "cats-and-dogs-backend"
-    namespace = "${kubernetes_namespace.cats-and-dogs.metadata.0.name}"
+    namespace = "cats-and-dogs"
   }
   spec {
     selector {
@@ -101,13 +101,13 @@ resource "kubernetes_service" "cats-and-dogs-backend" {
 resource "kubernetes_pod" "cats-and-dogs-frontend" {
   metadata {
     name = "cats-and-dogs-frontend"
-    namespace = "${kubernetes_namespace.cats-and-dogs.metadata.0.name}"
+    namespace = "cats-and-dogs"
     labels {
       App = "cats-and-dogs-frontend"
     }
   }
   spec {
-    service_account_name = "${kubernetes_service_account.cats-and-dogs.metadata.0.name}"
+    service_account_name = "cats-and-dogs"
     container {
       image = "rberlind/cats-and-dogs:latest"
       name  = "cats-and-dogs-frontend"
@@ -123,7 +123,7 @@ resource "kubernetes_pod" "cats-and-dogs-frontend" {
         name = "K8S_TOKEN"
         value_from {
           secret_key_ref {
-            name = "${kubernetes_service_account.cats-and-dogs.default_secret_name}"
+            name = "cats-and-dogs-token-mjbl9"
             key = "token"
           }
         }
@@ -140,7 +140,7 @@ resource "kubernetes_pod" "cats-and-dogs-frontend" {
 resource "kubernetes_service" "cats-and-dogs-frontend" {
   metadata {
     name = "cats-and-dogs-frontend"
-    namespace = "${kubernetes_namespace.cats-and-dogs.metadata.0.name}"
+    namespace = "cats-and-dogs"
   }
   spec {
     selector {
