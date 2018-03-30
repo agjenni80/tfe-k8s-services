4f48e1fb2d98d0b65c6427442752718a1e42152d
Roger Berlind <roger@hashicorp.com>
2018-03-29 23:35:48 -0400

    use variable for vault_addr

--- variables.tf
+++ variables.tf
@@ -6,3 +6,7 @@ variable "tfe_organization" {
 variable "k8s_cluster_workspace" {
   description = "workspace to use for the k8s cluster"
 }
+
+variable "vault_addr" {
+  description = "Vault address including port"
+}
